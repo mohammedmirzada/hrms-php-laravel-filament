9394fd2e3d4aaf790185602c01d53dc5191de31e
Mohammed D Mirzada <mohammed.d.mirzada@gmail.com>
2026-04-08 14:24:53 +0300

    Report & Bugs

--- database/settings/2026_03_28_073413_create_general_settings.php
+++ database/settings/2026_03_28_073413_create_general_settings.php
@@ -7,7 +7,7 @@ return new class extends SettingsMigration
     public function up(): void
     {
         $this->migrator->add('general.default_currency', 'USD');
-        $this->migrator->add('general.shortcuts', '[""]');
+        $this->migrator->add('general.shortcuts', '[]');
         $this->migrator->add('general.default_language', 'en');
     }
 };
