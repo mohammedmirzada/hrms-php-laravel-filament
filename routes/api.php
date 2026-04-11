6df793234226693ff953996aa486522041c17cdd
Mohammed D Mirzada <mohammed.d.mirzada@gmail.com>
2026-04-11 12:01:08 +0300

    Update Hikvision events route to include full path and enhance logging details

--- routes/api.php
+++ routes/api.php
@@ -12,11 +12,14 @@ use Illuminate\Http\Request;
 
 Route::prefix('hikvision')->group(function () {
 
-    Route::post('/events', function (Request $request) {
+    Route::post('/hikvision/events', function (Request $request) {
         Log::info('Hikvision Event Received', [
             'headers' => $request->headers->all(),
             'body' => $request->getContent(),
+            'all' => $request->all(),
+            'raw' => file_get_contents('php://input'),
         ]);
+        
         return response('OK', 200);
     });
 
