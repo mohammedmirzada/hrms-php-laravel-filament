7a5e7671b1142346ec9b431ab890cd37874c61a1
Mohammed D Mirzada <mohammed.d.mirzada@gmail.com>
2026-08-22 11:34:55 +0300

    new

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::match(['get', 'post'], '/iclock/cdata', function (\Illuminate\Http\Request $request) {
+    \Illuminate\Support\Facades\Log::info('ZK cdata', [
+        'method' => $request->method(),
+        'query'  => $request->query(),
+        'body'   => $request->getContent(),
+    ]);
+    return response('OK');
+});
+
+Route::match(['get', 'post'], '/iclock/getrequest', function () {
+    return response('OK');
+});
